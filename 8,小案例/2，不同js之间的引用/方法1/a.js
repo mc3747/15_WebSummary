b49a368a4b2b202b7502be47e2bc39d5ba3d8226
mc3747 <mc3747@126.com>
2019-07-29 18:25:09 +0800

    引用

---
+++ 8,小案例/2，不同js之间的引用/方法1/a.js
@@ -0,0 +1,6 @@
+
+document.write("<script type='text/javascript' src='b.js'></script>");
+function a(){
+	test();
+	console.log('aaa');
+}
---
+++ 8,小案例/2，不同js之间的引用/方法2/aa.js
@@ -0,0 +1,15 @@
+// var newscript = document.createElement('script');
+// newscript.setAttribute('type','text/javascript');
+// newscript.setAttribute('src','bb.js');
+// var head = document.getElementsByTagName('head')[0];
+// head.appendChild(newscript);
+
+
+function aa() {
+	var script = document.createElement('script');
+	script.setAttribute('type', 'text/javascript');
+	script.setAttribute('src', 'bb.js');
+	document.getElementsByTagName('head')[0].appendChild(script);
+	// test();
+	console.log('aaa');
+}
