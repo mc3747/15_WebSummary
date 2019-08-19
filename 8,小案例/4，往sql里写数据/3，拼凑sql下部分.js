3c57d41526e0ddb0db1b64f5b6bdbef947d30aa7
mc3747 <mc3747@126.com>
2019-08-19 15:45:23 +0800

    sql

--- 8,小案例/4，往sql里写数据/3，拼凑sql下部分.js
+++ 8,小案例/4，往sql里写数据/3，拼凑sql下部分.js
@@ -1,6 +1,6 @@
 var string = 'WHERE id IN (';
-	var first = 333;
-	var last = 355;
+	var first = 402;
+	var last = 431;
 	for(var i = first; i <= last; i++) {
 		if (i<last) {
 			string = string + i + ','
