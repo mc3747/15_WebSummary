3276d1bca2a29488fd5bf9e3374d1b648184b571
mc3747 <mc3747@126.com>
2019-08-27 15:33:36 +0800

    说明

--- 8,小案例/4，往sql里写数据/3，拼凑sql下部分.js
+++ 8,小案例/4，往sql里写数据/3，拼凑sql下部分.js
@@ -1,6 +1,6 @@
 var string = 'WHERE id IN (';
-	var first = 402;
-	var last = 431;
+	var first = 120;
+	var last = 155;
 	for(var i = first; i <= last; i++) {
 		if (i<last) {
 			string = string + i + ','
@@ -10,3 +10,5 @@ var string = 'WHERE id IN (';
 		
 	};
 	console.log(string);
+	
+//⚠️：此处结果要和上半部分一起组装成sql语句
