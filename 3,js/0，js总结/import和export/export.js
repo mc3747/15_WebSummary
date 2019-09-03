93baac2b46a369f048b08a2666cecf656932499f
mc3747 <mc3747@126.com>
2019-09-03 16:10:11 +0800

    切割

--- 3,js/0，js总结/import和export/export.js
+++ 3,js/0，js总结/import和export/export.js
@@ -2,14 +2,14 @@
 
 // ✅，1，输出变量
 // 方式1：变量直接export
-export let a1 = 100;
+// export let a1 = 100;
 
 // 方式2：变量直接
-var m = 1;
-export {m};
+// var m = 1;
+// export {m};
 
 // 方式3：变量直接:n来代替m
-export {m as n};
+// export {m as n};
 
 /*
 // 报错
@@ -20,20 +20,27 @@ var m = 1;
 export m;
 */ 
 // ✅， 2，变量统一输出
-let a2 = 200;
-let a3 = 'aaa';
-export{a2,a3};
+// let a2 = 200;
+// let a3 = 'aaa';
+// export{a2,a3};
 
 
 // ✅，3, 函数使用别名export
-function f1(a,b){
-	return a + b
-}
-export{f1};
-
-function f2(a,b){
-	return a * b
-}
-export{f2 as F2};
+// function f1(a,b){
+// 	return a + b
+// }
+// export{f1};
+// 
+// function f2(a,b){
+// 	return a * b
+// }
+// export{f2 as F2};
 
 // ✅，4, default输出
+//可以是匿名函数，
+// export default function () {
+// 	console.log('foo');
+// }
+
+let DefaultString = '哈哈';
+export default DefaultString ;
--- 3,js/0，js总结/import和export/import.js
+++ 3,js/0，js总结/import和export/import.js
@@ -6,6 +6,26 @@
 	// console.log(F2(5,4));
 	
 // ✅ 引用方式2：整体导入
-import * as AAA from './export.js';
-console.log(AAA.a1);
-console.log(AAA.f1(3,4));
+// import * as AAA from './export.js';
+// console.log(AAA.a1);
+// console.log(AAA.f1(3,4));
+
+//import default模块时
+/*
+// 第一组
+export default function crc32() { // 输出
+	// ...
+}
+import crc32 from 'crc32'; // 输入
+
+// 第二组
+export function crc32() { // 输出
+	// ...
+};
+import {crc32} from 'crc32'; // 输入
+*/
+// import aa from './export.js'
+// aa();
+
+import DefaultString from './export.js';
+console.log(DefaultString)
