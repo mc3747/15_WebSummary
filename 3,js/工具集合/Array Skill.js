31d995dcba12be2f74f3bd12cae61a52aa32a89f
mc3747 <mc3747@126.com>
2019-11-14 18:55:43 +0800

    标签

--- 3,js/工具集合/Array Skill.js
+++ 3,js/工具集合/Array Skill.js
@@ -103,8 +103,69 @@ console.log(arr17,arr18);
 // arr => [4]
 
 
-//13，删除数组的某一个元素
+//13，删除数组的某一个元素（已知下标）
 /*
 splice(index,len,[item]) 
 splice有3个参数，它也可以用来替换/删除/添加数组内某一个或者几个值
 */
+
+//14，删除数组的某一个元素（未知下标）
+Array.prototype.indexOf = function (val) {
+ for(var i = 0; i < this.length; i++){
+	if(this[i] == val){return i;}
+ }
+ return -1;
+}
+Array.prototype.remove = function (val) {
+ var index = this.indexOf(val);
+ if(index > -1){this.splice(index,1);}
+}
+var temp = [1,5,6,12,453,324];
+console.log('删除前'+temp);
+temp.remove(12);
+console.log('删除后'+temp);
+
+//15，判断某个元素是否在数组中
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+//方法1： arr.indexOf(某元素)：未找到则返回 -1。 
+var a = fruits.indexOf("Apple");  // 2
+//方法2：array.indexOf(item,start) ,从第n个元素开始检索
+var b = fruits.indexOf("Apple",4);  // 2
+//方法3：通过回调函数find，遍历
+fruits.find(function(value, index, arr) {
+	if(value == 'apple') ;
+}) // 10
+//方法4：通过回调函数findIndex，遍历
+fruits.findIndex(function(value, index, arr) {
+	if(value == 1);
+})
+//方法5，6，7：使用自定义遍历
+var arr = [1, 5, 10, 15];
+//传统for
+for(let i=0; i<fruits.length; i++) {
+	if(arr[i] === 'apple') {
+		//则包含该元素
+	}
+}
+// for...of
+for(v of fruits) {
+	if(v === 'apple') {
+		//则包含该元素
+	}
+}
+//forEach
+fruits.forEach(v=>{
+	if(v === 'apple') {
+		//则包含该元素
+	}
+//方法8: 自定义封装
+function isInArray3(arr,value){
+	if(arr.indexOf&&typeof(arr.indexOf)=='function'){
+		var index = arr.indexOf(value);
+		if(index >= 0){
+			return true;
+		}
+	}
+	return false;
+}
+
