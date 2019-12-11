61317b0b4255270c0d7979a7b13771ad11f68767
mc3747 <mc3747@126.com>
2019-12-11 18:35:06 +0800

    html

--- 3,js/🍎 1，js基础/🍏 13，类和模块/11，es6中的module的import.js
+++
@@ -1,9 +0,0 @@
-/*
-详见html版本的示例：
-export，import命令
-as命令
-
-
-*/
-import {firstName, lastName, year} from './12，es6中module的的export.js';
-console.log(firstName)
---
+++ 🍎 8,小案例/2，不同js之间的引用/方法4：import/a6.js
@@ -0,0 +1,5 @@
+
+import { firstName, lastName, year } from './b6.js';
+import Log from './b6.js/index.js';
+console.log(firstName);
+Log;
